a4bf52990c7f0a6eb1234ba4772abf5655fdca1e
Vishi234 <33386171+Vishi234@users.noreply.github.com>
2019-06-26 19:13:02 +0530

    Changes by Vishal

--- Product/App/src/app/app.module.ts
+++ Product/App/src/app/app.module.ts
@@ -4,10 +4,12 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
