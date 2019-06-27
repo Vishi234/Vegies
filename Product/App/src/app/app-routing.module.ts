72e4ad51955c606dfeae3d286ab042f9266bd049
Vishi234 <33386171+Vishi234@users.noreply.github.com>
2019-06-27 18:54:00 +0530

    Changes by Vishal

--- Product/App/src/app/app-routing.module.ts
+++ Product/App/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './visitor/home/home.component';
+import { ContainerComponent } from './visitor/container/container.component';
+import { RegisterComponent } from './vendor/register/register.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'index', pathMatch: 'full' },
-  { path: 'index', component: HomeComponent }
+  { 
+    path: '', 
+    component: ContainerComponent,
+    children: [
+      { path: '', redirectTo:'index', pathMatch: 'full'},
+      { path: 'index', component: HomeComponent},
+    ]
+},
+  { path: 'vendor/register', component:RegisterComponent},
 ];
 
 @NgModule({
--- Product/App/src/app/app.module.ts
+++ Product/App/src/app/app.module.ts
@@ -1,24 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MatInputModule,ErrorStateMatcher,ShowOnDirtyErrorStateMatcher, MatButtonModule, MatSelectModule, MatIconModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { HomeComponent } from './home/home.component';
+import { TopHeaderComponent } from './visitor/top-header/top-header.component';
+import { HeaderComponent } from './visitor/header/header.component';
+import { SidebarComponent } from './visitor/sidebar/sidebar.component';
+import { ContainerComponent } from './visitor/container/container.component';
+import { HomeComponent } from './visitor/home/home.component';
+import { RegisterComponent } from './vendor/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TopHeaderComponent,
+    HeaderComponent,
     SidebarComponent,
+    ContainerComponent,
     HomeComponent,
+    RegisterComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    FormsModule,    
+    MatInputModule, 
+    MatButtonModule,
+    MatSelectModule,
+    MatIconModule
   ],
-  providers: [],
+  providers: [
+    {provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
