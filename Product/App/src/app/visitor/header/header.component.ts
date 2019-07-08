c71c629722f34b675af67b1f1fce72a5d74c668c
sunilnittian <47137520+sunilnittian@users.noreply.github.com>
2019-07-08 18:01:03 +0530

    sdf

--- Product/App/src/app/visitor/header/header.component.ts
+++ Product/App/src/app/visitor/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material';
 import { RegisterComponent } from '../../vendor/register/register.component';
 import { LoginComponent } from '../../vendor/login/login.component';
+import { AdminCategoryService } from '../../admin/category/admin-category.service'
 
 @Component({
   selector: 'app-header',
@@ -10,8 +11,9 @@ import { LoginComponent } from '../../vendor/login/login.component';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(public dialog: MatDialog) { }
-
+  //constructor(public dialog: MatDialog) { }
+  constructor(private _adminCategory: AdminCategoryService,public dialog: MatDialog) { }
+  
   OpenRegisterModal() {
     this.dialog.open(RegisterComponent, { disableClose: true })
   }
@@ -19,7 +21,35 @@ export class HeaderComponent implements OnInit {
     this.dialog.open(LoginComponent, { disableClose: true })
   }
   
-  ngOnInit() {
+  public catList:any;
+  subCatList:any;
+  filtersLoaded: Promise<boolean>;
+
+  ngOnInit() 
+  {
+
+    this._adminCategory.GetCategoryList().subscribe((response) => 
+    {
+      var result = Object.keys(response).map(function (key) 
+      {
+        return [Number(key), response[key]];
+      });
+      this.catList = response["recordsets"][0].map((x:any)=>x.CATEGORY_NAME) ;
+      console.log("hiiiiiiiiii",this.catList)
+     
+      //console.log('Category List-->',this.catList[0].CATEGORY_NAME);
+      this.filtersLoaded = Promise.resolve(true);
+     }, (error) => {
+      console.log('error is ', error)
+    });
+
+    this._adminCategory.GetSubCategoryList().subscribe((response) => 
+    {
+     this.subCatList= response["recordset"];
+     }, (error) => {
+      console.log('error is ', error)
+    });
+
   }
 }
 
