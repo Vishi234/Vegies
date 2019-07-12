098bc793c7af308762a7db41c78ac926b7a5d328
Vishi234 <33386171+Vishi234@users.noreply.github.com>
2019-07-12 19:00:05 +0530

    Changes by Vishal

--- Product/App/src/app/vendor/configurationwizard/configurationwizard.component.ts
+++ Product/App/src/app/vendor/configurationwizard/configurationwizard.component.ts
@@ -1,9 +1,5 @@
 ///<reference types="@types/googlemaps" />
-import {Title} from '@angular/platform-browser';
 import { Component, OnInit, ViewEncapsulation} from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
 import {Location, Appearance} from '@angular-material-extensions/google-maps-autocomplete';
 import PlaceResult = google.maps.places.PlaceResult;
 
@@ -21,51 +17,12 @@ export class ConfigurationwizardComponent implements OnInit {
   public selectedAddress: PlaceResult;
 
   constructor(
-    private _formBuilder: FormBuilder,
-    private titleService: Title
   ) { }
   google : any;
-
-  toppings = new FormControl();
-  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-  isOptional = false;
-
-  selectFormControl = new FormControl();
-  searchTextboxControl = new FormControl();
-  selectedValues = [];
-  data: string[] = [
-    'A1',
-    'A2',
-    'A3',
-    'B1',
-    'B2',
-    'B3',
-    'C1',
-    'C2',
-    'C3'
-  ]
-  filteredOptions: Observable<any[]>;
-
   ngOnInit() {
-
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: '',//['',Validators.required]
-    });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ''
-    });
-    this.filteredOptions = this.searchTextboxControl.valueChanges
-      .pipe(
-        startWith<string>(''),
-        map(name => this._filter(name))
-      );
-    this.titleService.setTitle('Home | @angular-material-extensions/google-maps-autocomplete');
     this.zoom = 10;
     this.latitude = 52.520008;
     this.longitude = 13.404954;
- 
     this.setCurrentPosition();
   }
   private setCurrentPosition() {
@@ -86,21 +43,72 @@ export class ConfigurationwizardComponent implements OnInit {
     this.latitude = location.latitude;
     this.longitude = location.longitude;
   }
-  private _filter(name: string): String[] {
-    const filterValue = name.toLowerCase();
-    this.setSelectedValues();
-    this.selectFormControl.patchValue(this.selectedValues);
-    let filteredList = this.data.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
-    
-    return filteredList
-  }
-  setSelectedValues() {
-    if (this.selectFormControl.value && this.selectFormControl.value.length > 0) {
-      this.selectFormControl.value.forEach((e) => {
-        if (this.selectedValues.indexOf(e) == -1) {
-          this.selectedValues.push(e);
-        }
-      });
+  title = 'OwlCarousel2 in Angular7 with Custom Navigation Arrows';
+ 
+  carouselOptions = {
+    margin: 25,
+    nav: true,
+    navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 1,
+        nav: true
+      },
+      1000: {
+        items: 2,
+        nav: true,
+        loop: false
+      },
+      1500: {
+        items: 3,
+        nav: true,
+        loop: false
+      }
     }
   }
+ 
+  images = [
+    {
+      text: "Everfresh Flowers",
+      image: "https://freakyjolly.com/demo/jquery/PreloadJS/images/1.jpg"
+    },
+    {
+      text: "Festive Deer",
+      image: "https://freakyjolly.com/demo/jquery/PreloadJS/images/2.jpg"
+    },
+    {
+      text: "Morning Greens",
+      image: "https://freakyjolly.com/demo/jquery/PreloadJS/images/3.jpg"
+    },
+    {
+      text: "Bunch of Love",
+      image: "https://freakyjolly.com/demo/jquery/PreloadJS/images/4.jpg"
+    },
+    {
+      text: "Blue Clear",
+      image: "https://freakyjolly.com/demo/jquery/PreloadJS/images/5.jpg"
+    },
+    {
+      text: "Evening Clouds",
+      image: "https://freakyjolly.com/demo/jquery/PreloadJS/images/7.jpg"
+    },
+    {
+      text: "Fontains in Shadows",
+      image: "https://freakyjolly.com/demo/jquery/PreloadJS/images/8.jpg"
+    },
+    {
+      text: "Kites in the Sky",
+      image: "https://freakyjolly.com/demo/jquery/PreloadJS/images/9.jpg"
+    },
+    {
+      text: "Sun Streak",
+      image: "https://freakyjolly.com/demo/jquery/PreloadJS/images/10.jpg"
+    }
+  ]
+ 
 }
