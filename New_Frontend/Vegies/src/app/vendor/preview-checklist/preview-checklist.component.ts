0a3a036a6f8cee8b46a509cf670e14a8feb76bb2
Vishi234 <33386171+Vishi234@users.noreply.github.com>
2019-08-25 19:06:56 +0530

    Changes by Vishal

--- New_Frontend/Vegies/src/app/vendor/preview-checklist/preview-checklist.component.ts
+++ New_Frontend/Vegies/src/app/vendor/preview-checklist/preview-checklist.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {MAT_DIALOG_DATA} from '@angular/material';
-import { Inject } from '@angular/core';  
+import { MAT_DIALOG_DATA } from '@angular/material';
+import { Inject } from '@angular/core';
 import { MatDialog } from '@angular/material';
-
+import { SetAddressComponent } from '../set-address/set-address.component'
 @Component({
   selector: 'app-preview-checklist',
   templateUrl: './preview-checklist.component.html',
@@ -12,16 +12,17 @@ export class PreviewChecklistComponent implements OnInit {
 
   public myCheckList: any;
 
-  constructor( public dialog: MatDialog, @Inject(MAT_DIALOG_DATA,) public data: any) { }
+  constructor(public dialog: MatDialog, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
-  ngOnInit() 
-  {
-    console.log(this.data.ChecklistData);
-    this.myCheckList=this.data.ChecklistData;
-    
+  ngOnInit() {
+    this.myCheckList = this.data.ChecklistData;
+
   }
   closeModal() {
     this.dialog.closeAll();
+  }
+  changeAddress() {
+    this.dialog.open(SetAddressComponent, { disableClose: true })
   }
 }
 
